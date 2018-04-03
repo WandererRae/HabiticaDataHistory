af0469f88d698f150b9915c649ee8dbcf13a7430
Bogue Shannon <sb21477@wdx9000share11.jdnet.deere.com>
2018-04-03 15:42:40 -0500

    outline

---
+++ HabiticaDataHistory/HabiticaSignIn.swift
@@ -0,0 +1,35 @@
+//
+//  HabiticaSignIn.swift
+//  HabiticaDataHistory
+//
+//  Created by Bogue Shannon on 4/3/18.
+//  Copyright © 2018 WandererRae. All rights reserved.
+//
+
+import UIKit
+
+class HabiticaSignIn: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
